5f79a91242e89beddbf4a467f99ab0eeebf21687
Gerson <79485610+DIF21@users.noreply.github.com>
2022-02-09 18:26:42 -0300

    Revert "05 docker"

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
-# incluindo import os para adequar docker
-import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,7 +7,4 @@ def pagina_inicial():
     return "Hello World - Gerson SC"
 
 if __name__ == '__main__':
-    # incluindo import os para adequar docker
-    port = os.getenv('PORT')
-    # incluido '0.0.0.0', port=port para adequar docker
-    app.run('0.0.0.0', port=port)
+    app.run()
